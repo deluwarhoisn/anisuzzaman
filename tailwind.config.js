e81f802fbe34b9d697c7502600b07dc717ed42b4
deluwarhoisn <babuhossen301@gmail.com>
2025-08-02 13:51:45 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#000000',
+
+      },
+    
+    },
+  },
+  plugins: [],
+}
